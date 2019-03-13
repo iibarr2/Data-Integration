8bd6cbd6fa853b3b2d051c3b1e554324390d0956
Thapa, Bikal <bthapa1@ilstu.edugit>
2019-03-13 17:44:50 -0500

    read file and store in 2d array

--- src/Driver.java
+++ src/Driver.java
@@ -3,7 +3,12 @@
  */
 
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 /**
  * @author israe
@@ -15,11 +20,16 @@ import java.util.Scanner;
  */
 public class Driver {
 
+	static String[][] firstDataIn = new String[150][3];
+	public static int firstDataInSize = 0;
+	public boolean isThereAFile = false;
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
 		
 		Scanner userInput = new Scanner(System.in);
 		
@@ -41,13 +51,40 @@ public class Driver {
 							+ "('T' or 't') Save data for medical terms\n"
 							+ "('E' or 'e') Exit Program\n"
 							+ "\nEnter choice from above: ");
+	
+			
+			
 			
 			/*
 			 * grab the userinput and call methods for what they chose
 			 * from the above
 			 */
-			String choice = userInput.nextLine();
+			char choice = userInput.nextLine().charAt(0);
+
+			switch(choice) {
+				
+			case 'A':
+			case 'a':
+				System.out.println("Please enter file name: ");
+				getFileInfo(userInput.nextLine());
+				break;
 			
+			case 'R':
+			case 'r':
+				
+				break;
+				
+			case 'T':
+			case 't':
+				
+				break;
+				
+			case 'E':
+			case 'e':
+				
+				break;
+			
+			}
 			/*
 			 * start switch case statements
 			 */
@@ -56,5 +93,47 @@ public class Driver {
 		
 
 	}
+	
+	public static void getFileInfo(String fileName1) {
+		
+		File file = new File(fileName1);
+		String line32 = null;
+		try {
+		    Scanner scanner = new Scanner(file);
+
+		    //now read the file line by line...
+		    int lineNum = 0;
+		    int rowNum = 0;
+		    int colNum = 0;
+		    scanner.nextLine() ;
+		    while (scanner.hasNextLine()) {
+		        String line = scanner.nextLine();
+		        
+		        if(line.isEmpty()) {
+		        	continue;
+		        }
+		        String[] arrOfStr = line.split(","); 
+		        for (String element : arrOfStr) {
+		        	
+		        	firstDataIn[rowNum][colNum] = element;
+		        	colNum++;
+		            System.out.println(element);
+		        }
+		        if(colNum == 3) {
+		        	colNum = 0;
+		        }
+		        
+		        rowNum++;
+		        firstDataInSize++;
+		        lineNum++;
+		     
+		    }
+		} catch(FileNotFoundException e) { 
+		    System.out.println("No file found");
+		}
+		System.out.println(firstDataInSize);
+	}
+	
+	
 
 }
